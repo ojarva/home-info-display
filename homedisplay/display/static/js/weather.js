086db916adfeeb24783628f9a3848a32210e909a
Olli Jarva <olli.jarva@futurice.com>
2015-02-01 00:25:08 +0200

    Weather display improvements

--- homedisplay/display/static/js/weather.js
+++ homedisplay/display/static/js/weather.js
@@ -1,5 +1,4 @@
-
-$(document).ready(function () {
+var refresh_weather = function () {
   function set_weather_info (elem, text, info) {
     $(elem).html(text+"<br>"+info.fields.temperature+"&deg;C <img src='/homecontroller/static/images/"+info.fields.icon+".png'>");
   }
@@ -11,28 +10,44 @@ $(document).ready(function () {
   }
   $.get("/homecontroller/weather/get_json", function (data) {
     reset_weather_info();
-    var first_date = moment();
+    var first_date = moment(), plus_one = false;
     var hour = parseInt(first_date.format("H"));
     if (hour >= 23) {
       first_date = first_date.add(1, "days");
+      plus_one = true;
       hour = 0;
     }
     var today = first_date.format("YYYY-MM-DD");
     var tomorrow = first_date.add(1, "days").format("YYYY-MM-DD");
+    var desired_hours = {};
+    if (hour < 6) {
+      desired_hours[8] = {"this_elem": "#today .weather-2", "this_text": "Aamu"};
+      desired_hours[16] = {"this_elem": "#today .weather-3", "this_text": "Iltapäivä"};
+    } else if (hour < 12) {
+      desired_hours[16] = {"this_elem": "#today .weather-2", "this_text": "Iltapäivä"};
+      desired_hours[19] = {"this_elem": "#today .weather-3", "this_text": "Ilta"};
+    } else if (hour < 18) {
+      desired_hours[20] = {"this_elem": "#today .weather-2", "this_text": "Ilta"};
+      desired_hours[23] = {"this_elem": "#today .weather-3", "this_text": "Yö"};
+    }
     $.each(data, function () {
       var set = false;
       var this_text, this_elem;
-      console.log(this.fields.date, tomorrow, today);
+      this_data = this;
       if (this.fields.date == today) {
         // Current day
         if (this.fields.hour == hour) {
           set = true;
           this_elem = "#today .weather-1";
           this_text = "Nyt";
-        } else if (this.fields.hour == hour + 2) {
-          set = true;
-          this_elem = "#today .weather-2";
-          this_text = "2h";
+        } else {
+          for (var key in desired_hours) {
+            if (key == this.fields.hour) {
+              this_elem = desired_hours[key]["this_elem"];
+              this_text = desired_hours[key]["this_text"];
+              set = true;
+            }
+          }
         }
       } else if (this.fields.date == tomorrow) {
         // Next day
@@ -51,10 +66,16 @@ $(document).ready(function () {
         }
       }
       if (set) {
-        set_weather_info(this_elem, this_text, this);
+        if (plus_one) {
+          this_text += " (+1)";
+        }
+        set_weather_info(this_elem, this_text, this_data);
       }
-      console.log(this.fields.date);
     });
   });
+}
+$(document).ready(function () {
+  refresh_weather();
+  setInterval(refresh_weather, 300000); // 5 minutes
 
 });
