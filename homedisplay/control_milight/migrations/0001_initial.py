afdfe569a5c2d9ed9f356199a7769c4c28ae6ec0
Olli Jarva <olli.jarva@futurice.com>
2015-02-01 00:43:57 +0200

    Clean up broken migrations

--- homedisplay/control_milight/migrations/0001_initial.py
+++ homedisplay/control_milight/migrations/0001_initial.py
@@ -11,13 +11,32 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='LightState',
+            name='LightGroup',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('group', models.PositiveSmallIntegerField()),
-                ('rgbw_brightness', models.PositiveSmallIntegerField()),
-                ('white_brightness', models.PositiveSmallIntegerField()),
-                ('color', models.TextField()),
+                ('description', models.TextField(null=True, blank=True)),
+                ('rgbw_brightness', models.PositiveSmallIntegerField(null=True)),
+                ('white_brightness', models.PositiveSmallIntegerField(null=True)),
+                ('color', models.TextField(null=True, blank=True)),
+                ('on', models.NullBooleanField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='LightTransition',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('start_time', models.DateTimeField()),
+                ('end_time', models.DateTimeField()),
+                ('priority', models.PositiveSmallIntegerField(default=0)),
+                ('start_brightness', models.PositiveSmallIntegerField(null=True)),
+                ('to_brightness', models.PositiveSmallIntegerField(null=True)),
+                ('to_nightmode', models.NullBooleanField()),
+                ('to_color', models.TextField(null=True, blank=True)),
+                ('group', models.ForeignKey(to='control_milight.LightGroup')),
             ],
             options={
             },
