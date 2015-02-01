9939b573523bb70b76710a5410c2ebc5e2706281
Olli Jarva <olli.jarva@futurice.com>
2015-02-01 16:25:17 +0200

    /status returns list with single item.

--- homedisplay/info_internet_connection/static/js/internet_connection.js
+++ homedisplay/info_internet_connection/static/js/internet_connection.js
@@ -1,13 +1,14 @@
 function refresh_internet() {
-    $.get("/homecontroller/internet_connection/status", function (data) {
-        console.log(data);
-        output = $("#internet_connection");
-        output.find("#connected").html(data.fields.connect_status);
-        output.find("#mode").html(data.fields.mode);
-        output.find("#signal").html(data.fields.signal+"/5");
-        data_moment = moment(data.fields.timestamp);
-        output.find("#age").html(data_moment.fromnow());
-    });
+  $.get("/homecontroller/internet_connection/status", function (data) {
+    data = data[0];
+    console.log(data);
+    output = $("#internet_connection");
+    output.find("#connected").html(data.fields.connect_status);
+    output.find("#mode").html(data.fields.mode);
+    output.find("#signal").html(data.fields.signal+"/5");
+    data_moment = moment(data.fields.timestamp);
+    output.find("#age").html(data_moment.fromnow());
+  });
 }
 
 $(document).ready(function() {
