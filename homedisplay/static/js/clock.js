857cb222f5195400b595c2746eb382f9ee3e9330
Olli Jarva <olli.jarva@futurice.com>
2015-02-01 14:22:47 +0200

    Removed autocollected static files.

--- homedisplay/static/js/clock.js
+++
@@ -1,15 +0,0 @@
-function handleClock() {
-    var days = new Array("su", "ma", "ti", "ke", "to", "pe", "la");
-    var currentTime = new Date ( );
-    var currentDay = days[currentTime.getDay()];
-    var currentDate = currentTime.getDate();
-    var currentMonth = currentTime.getMonth()+1;
-    $("#calendar").html(currentDay+" "+currentDate+"."+currentMonth+".");
-    var currentTime = new Date ( );
-    var currentHours = currentTime.getHours ( );
-    var currentMinutes = currentTime.getMinutes ( );
-    var currentSeconds = currentTime.getSeconds ( );
-    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-    $("#clock").html(currentHours+":"+currentMinutes+":"+currentSeconds);
-}$(document).ready(function() {    handleClock();    setInterval("handleClock()", 1000); // 1s});
--- homedisplay/static/js/lightcontrol.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $.each($(".lightcontrol-btn"), function() {
-      $(this).on("click", function () {
-        var command = $(this).data("command");
-        var group = $(this).data("group") || "0";
-        $.get("/homecontroller/lightcontrol/control/"+command+"/"+group);
-      });
-  });
-});
