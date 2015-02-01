65bdc6f89c69f99abe9853f3c5b02ba3c4cc554b
Olli Jarva <olli.jarva@futurice.com>
2015-02-01 16:56:19 +0200

    Indoor quality updates

---
+++ homedisplay/indoor_quality/management/commands/fetch_indoor_quality.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.timezone import now
+
+from indoor_quality.models import IndoorQuality
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Fetches indoor air quality information'
+
+    def handle(self, *args, **options):
+        co2 = open("/mnt/owfs/20.F1580D000000/CO2/ppm").read()
+        temperature = open("/mnt/owfs/22.53B222000000/temperature").read()
+        a = IndoorQuality(co2=co2, temperature=temperature)
+        a.save()
--- homedisplay/indoor_quality/views.py
+++ homedisplay/indoor_quality/views.py
@@ -7,8 +7,5 @@ from .models import IndoorQuality
 
 class get_co2(View):
     def get(self, request, *args, **kwargs):
-        data = IndoorQuality.objects.all()[0:]
-        co2 = open("/mnt/owfs/20.F1580D000000/CO2/ppm").read()
-        temperature = open("/mnt/owfs/22.53B222000000/temperature").read()
-        data = {"co2": co2, "temperature": temperature}
+        data = IndoorQuality.objects.all()[0:1440]
         return HttpResponse(serializers.serialize("json", data), content_type="application/json")
