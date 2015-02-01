a20e6cb9431d83db167ed668af8b04200abbd3d9
Olli Jarva <olli.jarva@futurice.com>
2015-02-01 16:17:31 +0200

    Internet connection updates

--- homedisplay/info_internet_connection/static/js/internet_connection.js
+++ homedisplay/info_internet_connection/static/js/internet_connection.js
@@ -2,17 +2,15 @@ function refresh_internet() {
     $.get("/homecontroller/internet_connection/status", function (data) {
         console.log(data);
         output = $("#internet_connection");
-        output.find("#connected").html(data.connected);
-        output.find("#mode").html(data.mode);
-        output.find("#signal").html(data.signal+"/5");
-    //    $("#internet_connection")
+        output.find("#connected").html(data.fields.connect_status);
+        output.find("#mode").html(data.fields.mode);
+        output.find("#signal").html(data.fields.signal+"/5");
+        data_moment = moment(data.fields.timestamp);
+        output.find("#age").html(data_moment.fromnow());
     });
 }
 
 $(document).ready(function() {
-    $.get("/homecontroller/internet_connection/content", function(data) {
-        $("#internet_connection").html(data);
-        refresh_internet();
-        setInterval(refresh_internet, 60000);
-    });
+  refresh_internet();
+  setInterval(refresh_internet, 60000);
 });
--- homedisplay/info_internet_connection/views.py
+++ homedisplay/info_internet_connection/views.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
-import json
 import time
+from django.core import serializers
+
 
 from .models import Internet
 class info(View):
@@ -12,7 +13,4 @@ class info(View):
             data = Internet.objects.latest()
         except Internet.DoesNotExist:
             return HttpResponse(json.dumps({"status": "error", "message": "No data available"}))
-        age = int(time.mktime(data.update_timestamp.timetuple())*1000)
-
-        ret = {"connected": data.connect_status, "signal": data.signal, "mode": data.mode, "age": age }
-        return HttpResponse(json.dumps(ret), content_type="application/json")
+        return HttpResponse(serializers.serialize("json", data), content_type="application/json")
