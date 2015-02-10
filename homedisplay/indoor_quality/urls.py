9b0cab681a3cde42397b5b2df6afc7aea14f80c9
Olli Jarva <olli.jarva@futurice.com>
2015-02-10 21:38:11 +0200

    Fixes to URLs

--- homedisplay/indoor_quality/urls.py
+++ homedisplay/indoor_quality/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    (r'^get_json/(?P<sensor_id>(.+)$', views.get_json.as_view()),
-    (r'^get_json/(?P<sensor_id>(.+)/trend$', views.get_json_trend.as_view()),
+    (r'^get_json/(?P<sensor_id>([a-z0-9-_]+))$', views.get_json.as_view()),
+    (r'^get_json/(?P<sensor_id>([a-z0-9-_]+))/trend$', views.get_json_trend.as_view()),
 )
