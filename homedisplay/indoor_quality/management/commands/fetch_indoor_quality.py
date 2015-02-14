7d71afd01c88dc73d1fcc07bf2134c123619c738
Olli Jarva <olli.jarva@futurice.com>
2015-02-14 17:35:14 +0200

    Add automatic caching for averaged air quality data
    This implements redis caching for past 24 hours of air quality data.
    
    Every time datapoint is added, redis cache is updated. Data is stored in
    serialized json, which can be either deserialized or pushed directly to
    the browser.

--- homedisplay/indoor_quality/management/commands/fetch_indoor_quality.py
+++ homedisplay/indoor_quality/management/commands/fetch_indoor_quality.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.timezone import now
-from indoor_quality.models import IndoorQuality
 import redis
 r = redis.StrictRedis()
 
--- homedisplay/indoor_quality/models.py
+++ homedisplay/indoor_quality/models.py
@@ -1,13 +1,8 @@
 from django.db import models
+import redis
+import json
 
-class IndoorQuality(models.Model):
-    class Meta:
-        ordering = ["-timestamp"]
-        get_latest_by = "-timestamp"
-
-    timestamp = models.DateTimeField(auto_now_add=True)
-    co2 = models.IntegerField(null=True)
-    temperature = models.DecimalField(max_digits=6, decimal_places=2)
+redis_instance = redis.StrictRedis()
 
 class AirDataPoint(models.Model):
     timepoint = models.ForeignKey("AirTimePoint")
@@ -17,6 +12,29 @@ class AirDataPoint(models.Model):
     class Meta:
         ordering = ["-timepoint__timestamp"]
         get_latest_by = "-timepoint__timestamp"
+
+    def save(self, *args, **kwargs):
+        redis_collect_key = "air-collect-%s" % self.name
+        redis_storage_key = "air-storage-%s" % self.name
+        redis_instance.lpush(redis_collect_key, self.value)
+        if self.timepoint.timestamp.minute % 5 == 0:
+            # Calculate average every five minutes
+            old_values = redis_instance.lrange(redis_collect_key, 0, -1)
+            redis_instance.delete(redis_collect_key)
+            final_values = []
+            for item in old_values:
+              try:
+                final_values.append(float(item))
+              except ValueError:
+                pass
+            if len(final_values) > 0:
+              value = float(sum(final_values)) / len(final_values)
+            else:
+              value = None
+
+            redis_instance.lpush(redis_storage_key, json.dumps({"timestamp": str(self.timepoint.timestamp), "value": value}))
+            redis_instance.ltrim(redis_storage_key, 0, 288)
+        super(AirDataPoint, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return "%s - %s=%s" % (self.timepoint.timestamp, self.name, self.value)
