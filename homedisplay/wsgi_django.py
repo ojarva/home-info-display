8707134e14cd8c8b14401013adcdb90cb2036354
Olli Jarva <olli.jarva@futurice.com>
2015-02-18 16:46:36 +0200

    Update WSGI configurations to match production

--- homedisplay/wsgi_django.py
+++ homedisplay/wsgi_django.py
@@ -1,3 +1,5 @@
 # entry point for the Django loop
+import os
+os.environ.update(DJANGO_SETTINGS_MODULE='homedisplay.settings')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
--- homedisplay/wsgi_websocket.py
+++ homedisplay/wsgi_websocket.py
@@ -1,6 +1,8 @@
 # entry point for the websocket loop
-import gevent.monkey
+import os
+import gevent.socket
 import redis.connection
 redis.connection.socket = gevent.socket
+os.environ.update(DJANGO_SETTINGS_MODULE='homedisplay.settings')
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 application = uWSGIWebsocketServer()
