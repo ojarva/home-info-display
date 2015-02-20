e5fe4052c6b34a87c5045dba06bbf51bf5731251
Olli Jarva <olli.jarva@futurice.com>
2015-02-20 21:07:13 +0200

    Refactor weather display

--- homedisplay/info_weather/static/js/weather.js
+++ homedisplay/info_weather/static/js/weather.js
@@ -8,79 +8,37 @@ var RefreshWeather = function (options) {
   }
 
   function resetWeatherInfo() {
-    $(".weather-info .col-md-4").each(function () {
-      $(this).html("<br><i class='fa fa-question-circle'></i>");
+    $(".weather .weather-box span").each(function () {
+      $(this).html("<i class='fa fa-question-circle'></i>");
     });
   }
 
   function processData(data) {
     resetWeatherInfo();
-    var first_date = moment(), plus_one = false, hour, today, tomorrow, desired_hours = {};
-    hour = parseInt(first_date.format("H"));
-    if (hour >= 23) {
-      first_date = first_date.add(1, "days");
-      plus_one = true;
-      hour = 0;
-    }
-    today = first_date.format("YYYY-MM-DD");
-    tomorrow = first_date.add(1, "days").format("YYYY-MM-DD");
-    desired_hours = {};
-    if (hour < 6) {
-      desired_hours[8] = {"this_elem": "#today .weather-2", "this_text": "Aamu"};
-      desired_hours[16] = {"this_elem": "#today .weather-3", "this_text": "Iltapäivä"};
-    } else if (hour < 12) {
-      desired_hours[16] = {"this_elem": "#today .weather-2", "this_text": "Iltapäivä"};
-      desired_hours[19] = {"this_elem": "#today .weather-3", "this_text": "Ilta"};
-    } else if (hour < 18) {
-      desired_hours[20] = {"this_elem": "#today .weather-2", "this_text": "Ilta"};
-      desired_hours[23] = {"this_elem": "#today .weather-3", "this_text": "Yö"};
-    } else if (hour < 23) {
-      desired_hours[23] = {"this_elem": "#today .weather-2", "this_text": "Yö"};
-    }
     if (data.sun) {
       var sunrise = moment(data.sun.sunrise);
       var sunset = moment(data.sun.sunset);
       $(".sun-info").html("<i class='fa fa-sun-o'></i><i class='fa fa-long-arrow-up'></i> " + sunrise.format("HH:mm") + " (<span class='auto-fromnow-update' data-timestamp='" + sunrise + "'>" + sunrise.fromNow() + "</span>) <i class='fa fa-sun-o'></i><i class='fa fa-long-arrow-down'></i> " + sunset.format("HH:mm") + " (<span class='auto-fromnow-update' data-timestamp='" + sunset + "'>" + sunset.fromNow() + "</span>)");
     }
-    $.each(data.hours, function () {
-      var set = false, this_text, this_elem, this_data = this;
-      if (this.fields.date == today) {
-        // Current day
-        if (this.fields.hour == hour) {
-          set = true;
-          this_elem = "#today .weather-1";
-          this_text = "Nyt";
-        } else {
-          for (var key in desired_hours) {
-            if (key == this.fields.hour) {
-              this_elem = desired_hours[key].this_elem;
-              this_text = desired_hours[key].this_text;
-              set = true;
-            }
-          }
-        }
-      } else if (this.fields.date == tomorrow) {
-        // Next day
-        if (this.fields.hour == 8) {
-          set = true;
-          this_elem = "#tomorrow .weather-1";
-          this_text = "Aamu";
-        } else if (this.fields.hour == 16) {
-          set = true;
-          this_elem = "#tomorrow .weather-2";
-          this_text = "Iltapäivä";
-        } else if (this.fields.hour > 18) {
-          set = true;
-          this_elem = "#tomorrow .weather-3";
-          this_text = "Ilta";
+    var items = $(".weather"),
+        current_index = 1,
+        first_date = false,
+        new_day = false;
+    $.each(data.next, function () {
+      var this_item = items.find(".weather-" + current_index);
+      if (first_date === false) {
+        first_date = this.item.date;
+      } else if (new_day === false) {
+        if (first_date !== this.item.date) {
+          this_item.addClass("new-day");
+          new_day = true;
         }
       }
-      if (set) {
-        if (plus_one) {
-          this_text += " (+1)";
-        }
-        setWeatherInfo(this_elem, this_text, this_data);
-      }
+      this_item.find(".timestamp").html(this.name);
+      this_item.find(".temperature").html(this.item.apparent_temperature);
+      this_item.find(".symbol").html("<img src='/homecontroller/static/images/" + this.item.icon + ".png'>");
+      this_item.find(".temperature-unit").html("&deg;C");
+      current_index += 1;
     });
   }
 
--- homedisplay/info_weather/views.py
+++ homedisplay/info_weather/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from .models import Weather
 from astral import Astral
 from django.conf import settings
@@ -11,6 +13,16 @@ import datetime
 import json
 import subprocess
 import time
+from django.forms.models import model_to_dict
+
+
+def json_default(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    if isinstance(obj, datetime.date):
+        return obj.isoformat()
+    if isinstance(obj, datetime.time):
+        return obj.isoformat()
 
 def calculate_apparent_temperature(temperature, wind, humidity):
     wind /= 3.6
@@ -36,11 +48,14 @@ def get_wind_readable(wind):
         return "myrsky"
 
 def get_weather_data():
-    time_now = now()
-    forecast = {"hours": json.loads(serializers.serialize("json", Weather.objects.filter(date__gte=time_now.date())))}
-    for item in forecast["hours"]:
-        item["fields"]["apparent_temperature"] = calculate_apparent_temperature(item["fields"]["temperature"], float(item["fields"]["wind_speed"]), item["fields"]["humidity"])
-        item["fields"]["wind_speed_readable"] = get_wind_readable(float(item["fields"]["wind_speed"]))
+    time_now = now().replace(minute=0, second=0, microsecond=0)
+    weather_objects = Weather.objects.filter(date__gte=time_now.date())
+    forecast = {"hours": []}
+    for item in weather_objects:
+        i = model_to_dict(item)
+        i["apparent_temperature"] = calculate_apparent_temperature(item.temperature, float(item.wind_speed), item.humidity)
+        i["wind_speed_readable"] = get_wind_readable(float(item.wind_speed))
+        forecast["hours"].append(i)
 
     sun_info = Astral()
     sun_info.solar_depression = 'civil'
@@ -48,6 +63,33 @@ def get_weather_data():
     for k in b:
         b[k] = unicode(b[k])
     forecast["sun"] = b
+    forecast["next"] = []
+
+    time_now = datetime.datetime.now()
+    hour = time_now.hour
+    desired_entries = [("+2h", time_now + datetime.timedelta(hours=2))]
+    if hour <= 4:
+        # Show +2h, morning, midday, evening
+        desired_entries += [("Aamu", time_now.replace(hour=8)), ("Päivä", time_now.replace(hour=13)), ("Ilta", time_now.replace(hour=19))]
+    elif hour <= 10:
+        # Show +2h, afternoon, evening, next morning
+        desired_entries += [("Päivä", time_now.replace(hour=14)), ("Ilta", time_now.replace(hour=19)), ("Aamu", time_now.replace(hour=8) + datetime.timedelta(days=1))]
+    elif hour <= 18:
+        # Show +2h, evening, next morning, next afternoon
+        desired_entries += [("Ilta", time_now.replace(hour=21)), ("Aamu", time_now.replace(hour=8) + datetime.timedelta(days=1)), ("Päivä", time_now.replace(hour=14) + datetime.timedelta(days=1))]
+    elif hour <= 21:
+        # Show +2h, night, next morning, next afternoon
+        desired_entries += [("Yö", time_now.replace(hour=1) + datetime.timedelta(days=1)),  ("Aamu", time_now.replace(hour=8) + datetime.timedelta(days=1)), ("Päivä", time_now.replace(hour=14) + datetime.timedelta(days=1))]
+    else:
+        # Show +2h, late night, next morning, next afternoon
+        desired_entries += [("Yö", time_now.replace(hour=3) + datetime.timedelta(days=1)),  ("Aamu", time_now.replace(hour=8) + datetime.timedelta(days=1)), ("Päivä", time_now.replace(hour=14) + datetime.timedelta(days=1))]
+    for time_name, timestamp in desired_entries:
+        for weather_item in forecast["hours"]:
+            if weather_item["date"] == timestamp.date():
+                if weather_item["hour"] == timestamp.hour:
+                    forecast["next"].append({"name": time_name, "item": weather_item})
+                    break
+    forecast = json.loads(json.dumps(forecast, default=json_default))
     return forecast
 
 
