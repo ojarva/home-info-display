2ba3ea14cc738f204a6b337d874ae79d33c21a06
Olli Jarva <olli.jarva@futurice.com>
2015-02-21 10:10:56 +0200

    Format strings properly

--- homedisplay/control_milight/management/commands/run_timed.py
+++ homedisplay/control_milight/management/commands/run_timed.py
@@ -23,12 +23,12 @@ class Command(BaseCommand):
             percent_done = item.percent_done(time)
             if item.action == "evening" or item.action == "evening-weekend":
                 brightness = int((1-percent_done)*100)
-                print "Setting morning brightness to %s%" % brightness
+                print "Setting morning brightness to %s%%" % brightness
                 led.set_brightness(brightness)
                 update_lightstate(0, brightness)
             elif item.action == "morning" or item.action == "morning-weekend":
                 brightness = int(percent_done * 100)
-                print "Setting morning brightness to %s%" % brightness
+                print "Setting morning brightness to %s%%" % brightness
                 led.set_brightness(brightness)
                 led.white()
                 update_lightstate(0, brightness, "white")
