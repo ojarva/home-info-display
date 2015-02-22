6dd60d1bce73dcc394c2ea27188d39d894ac6fe7
Olli Jarva <olli.jarva@futurice.com>
2015-02-22 18:31:46 +0200

    Use sentry for error logging

--- homedisplay/wsgi_django.py
+++ homedisplay/wsgi_django.py
@@ -1,5 +1,13 @@
 # entry point for the Django loop
 import os
+from raven import Client
+from raven.middleware import Sentry
+
+from raven_settings import DSN
+
+client = Client(DSN)
+
 os.environ.update(DJANGO_SETTINGS_MODULE='homedisplay.settings')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+application = Sentry(application, client=client)
