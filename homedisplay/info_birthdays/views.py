e48d67eb741ed49d7595d127e512ae6ee4fdd571
Olli Jarva <olli@jarva.fi>
2015-02-26 11:23:47 +0200

    Clean up unnecessary imports

--- homedisplay/info_birthdays/views.py
+++ homedisplay/info_birthdays/views.py
@@ -1,15 +1,7 @@
-from .models import Birthday, get_birthdays
-from django.conf import settings
-from django.core import serializers
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.utils.decorators import method_decorator
-from django.utils.timezone import now
-from django.views.decorators.csrf import csrf_exempt
+from .models import get_birthdays
+from django.http import HttpResponse
 from django.views.generic import View
 import json
-import time
-import datetime
 
 class list(View):
     def get(self, request, *args, **kwargs):
