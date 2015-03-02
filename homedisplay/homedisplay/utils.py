f1b22c952dabb3b66638000078e1ab2d0b7acea2
Olli Jarva <olli@jarva.fi>
2015-03-02 18:21:49 +0200

    Add missing utils file
    Related to ed7bd7610e69b0702fac54443d250038f8489c42 and #92

---
+++ homedisplay/homedisplay/utils.py
@@ -0,0 +1,7 @@
+import redis
+import json
+
+redis_instance = redis.StrictRedis()
+
+def publish_ws(key, content):
+    redis_instance.publish("home:broadcast:generic", json.dumps({"key": key, "content": content}))
