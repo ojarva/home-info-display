f0e733a3f62d37dc25d70b334dd3e1e46936477d
Olli Jarva <olli@jarva.fi>
2015-03-04 23:08:11 +0200

    Add missing non-important migration

---
+++ homedisplay/info_transportation/migrations/0016_auto_20150304_2159.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('info_transportation', '0015_line_type'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='line',
+            name='type',
+            field=models.CharField(default=b'bus', help_text=b'Liikennev\xc3\xa4linetyyppi', max_length=10, verbose_name=b'Tyyppi', choices=[(b'bus', b'bus'), (b'tram', b'tram'), (b'train', b'train'), (b'metro', b'metro')]),
+            preserve_default=True,
+        ),
+    ]
