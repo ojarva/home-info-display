791e83f676e0c920f58a8278da144efadf56feb4
Olli Jarva <olli@jarva.fi>
2015-03-09 23:52:52 +0200

    Show current peers in wifi view

--- homedisplay/info_internet_connection/static/js/wifi.coffee
+++ homedisplay/info_internet_connection/static/js/wifi.coffee
@@ -1,16 +1,32 @@
+WifiInfo = ->
+  initialize = ->
+    # This shouldn't be called more than once
+    jq.get "/homecontroller/internet_connection/wifi/info", (data) ->
+      elem = jq ".wifi-info"
+      elem.find(".ssid").html data.ssid
+      elem.find(".passphrase").html data.passphrase
+      jq("#wifi-qr").qrcode
+        "text": "WIFI:S:" + data.ssid + ";T:WPA;P:" + data.passphrase + ";;"
+        "background": "white"
+        "render": "div"
+        "size": 600
+      return
 
+  updateUnifi = (data) ->
+    elem = jq "#wifi-clients"
+    elem.children().remove()
+    jq.each data.devices, ->
+      elem.append "<div class='wifi-client'><span class='mac'>" + this.mac + "</span> <span class='last-seen'>" + this.last_seen + "</span> <span class='label'>rssi:</span> <span class='rssi'>" + this.rssi + "</span><span class='unit'>dBm</span> <span class='bandwidth'>" + this.current_bandwidth + "/" + this.maximum_bandwidth + "</span><span class='label'>Mbit/s</span></div>"
+
+  ws_generic.multiRegister "unifi-status", "unifi-status-main", updateUnifi
+
+  initialize()
+  return this
+
+wifi_info = null
 
 jq ->
-  jq.get "/homecontroller/internet_connection/wifi/info", (data) ->
-    elem = jq(".wifi-info")
-    elem.find(".ssid").html data.ssid
-    elem.find(".passphrase").html data.passphrase
-    jq("#wifi-qr").qrcode
-      "text": "WIFI:S:" + data.ssid + ";T:WPA;P:" + data.passphrase + ";;"
-      "background": "white"
-      "render": "div"
-      "size": 600
-    return
+  wifi_info = new WifiInfo()
 
   jq(".main-button-box .open-wifi-settings").on "click", ->
     content_switch.switchContent "#wifi-settings"
---
+++ unifi_status_publisher/run.py
@@ -0,0 +1,62 @@
+from setproctitle import setproctitle
+import json
+import local_settings
+import paramiko
+import re
+import redis
+import subprocess
+import time
+
+class UnifiStatusPublisher(object):
+    stainfo_re = re.compile("(?P<timestamp>.{2,3})\[(?P<mac>.*)\] (?P<flags>.{3}) idle=(?P<idletime>[0-9 ]+)rssi=(?P<rssi>[0-9 ]+) (?P<current_bandwidth>[0-9 ]+)/(?P<maximum_bandwidth>[0-9 ]+)ccq=.*queued=(?P<tx>[0-9 ]+)/(?P<ret>[0-9 ]+)/(?P<queued>[0-9 ]+)rx=(?P<rx>[0-9 ]+)err=(?P<err>[0-9/ ]+)")
+
+    def __init__(self):
+        self.redis_instance = redis.StrictRedis()
+        self.client = paramiko.client.SSHClient()
+        self.client.load_system_host_keys()
+        self.client.connect(local_settings.UNIFI_IP, username=local_settings.UNIFI_USERNAME, password=local_settings.UNIFI_PASSWORD)
+
+    def poll_unifi_status(self):
+        stdin, stdout, stderr = self.client.exec_command("stainfo -a")
+        publish_data = {"devices": [], "stats": {}}
+        for line in stdout:
+            data = line.strip()
+            if data.startswith("- showing"):
+                # end of data row
+                self.redis_instance.publish("home:broadcast:generic", json.dumps({"key": "unifi-status", "content": publish_data}))
+                publish_data = {"devices": [], "stats": {}}
+
+                continue
+            if data.startswith("wifi"):
+                # Wifi info line
+                continue
+
+            matched = self.stainfo_re.match(data)
+            if data is None:
+                print "Invalid data: %s" % data
+                continue
+            item_data = {}
+            item_data["last_seen"] = matched.group("timestamp")
+            item_data["mac"] = matched.group("mac")
+            item_data["flags"] = matched.group("flags")
+            item_data["idle"] = int(matched.group("idletime").strip())
+            item_data["rssi"] = int(matched.group("rssi").strip())
+            item_data["current_bandwidth"] = int(matched.group("current_bandwidth").strip())
+            item_data["maximum_bandwidth"] = int(matched.group("maximum_bandwidth").strip())
+            item_data["rx"] = int(matched.group("rx").strip())
+            item_data["tx"] = int(matched.group("tx").strip())
+            publish_data["devices"].append(item_data)
+
+    def run(self):
+        while True:
+            self.poll_unifi_status()
+            break
+            time.sleep(1)
+
+def main():
+    setproctitle("unifi_status_publisher: run")
+    usp = UnifiStatusPublisher()
+    usp.run()
+
+if __name__ == '__main__':
+    main()
