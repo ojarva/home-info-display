6144e93d7f895e32ce2427ad0307a02a14d4e33f
Olli Jarva <olli@jarva.fi>
2015-03-09 20:58:11 +0200

    Implement wifi settings view

--- homedisplay/info_internet_connection/urls.py
+++ homedisplay/info_internet_connection/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
-    (r'^status', views.get_json.as_view()),
+    (r'^status', views.GetJson.as_view()),
+    (r'^wifi/info', views.WifiInfo.as_view()),
 )
--- homedisplay/info_internet_connection/views.py
+++ homedisplay/info_internet_connection/views.py
@@ -2,8 +2,17 @@ from .models import get_latest_serialized
 from django.core import serializers
 from django.http import HttpResponse
 from django.views.generic import View
+from django.conf import settings
 import json
 
-class get_json(View):
+class GetJson(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse(json.dumps(get_latest_serialized()), content_type="application/json")
+
+class WifiInfo(View):
+    def get(self, request, *args, **kwargs):
+        wifi_settings = {
+            "ssid": settings.WIFI_SSID,
+            "passphrase": settings.WIFI_PASSPHRASE
+        }
+        return HttpResponse(json.dumps(wifi_settings), content_type="application/json")
