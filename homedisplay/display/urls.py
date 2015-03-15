93cd2602e33e9508b1a144ebfa5cdce7d687e6f1
Olli Jarva <olli@jarva.fi>
2015-03-15 17:36:35 +0200

    Rename display template for consistency

--- homedisplay/display/urls.py
+++ homedisplay/display/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 import views
 
 urlpatterns = patterns('',
-    (r'^content/display$', TemplateView.as_view(template_name="index.html")),
+    (r'^content/display$', TemplateView.as_view(template_name="index_display.html")),
     (r'^content/kitchen$', TemplateView.as_view(template_name='index_kitchen.html')),
     (r'^content/door$', TemplateView.as_view(template_name='index_door.html')),
     (r'^content/computer$', TemplateView.as_view(template_name='index_computer.html')),
