41fae7b73867e09b9c3965b7b3cbb911b67a26c5
Olli Jarva <olli@jarva.fi>
2015-03-15 21:44:00 +0200

    Add missing (meta) migration from previous commit

---
+++ homedisplay/info_ext_pages/migrations/0002_auto_20150315_2143.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('info_ext_pages', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='extpage',
+            options={'ordering': ('timestamp',), 'get_latest_by': 'timestamp', 'verbose_name': 'Osoite', 'verbose_name_plural': 'Osoitteet'},
+        ),
+    ]
