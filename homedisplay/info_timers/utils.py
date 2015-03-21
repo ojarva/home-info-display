8f74255c1419211b468a8056d736abb40ebf69ec
Olli Jarva <olli@jarva.fi>
2015-03-21 18:36:13 +0200

    Improve logging and remove duplicate code

--- homedisplay/info_timers/utils.py
+++ homedisplay/info_timers/utils.py
@@ -2,7 +2,11 @@
 
 from django.utils import timezone
 import datetime
+import logging
 
+logger = logging.getLogger("%s.%s" % ("homecontroller", __name__))
+
+# TODO: move to local_settings
 GROUP_TIMER_NAME_MAP = {
     1: "Tatamin valot",
     2: "Pöydän valot",
@@ -18,10 +22,14 @@ def update_group_automatic_timer(group, on_until=None):
     else:
         duration = 10 * 60 # TODO: default
 
-    timer, created = timer_models.Timer.objects.get_or_create(action="auto-lightgroup-%s" % group, defaults={"name": GROUP_TIMER_NAME_MAP[group], "start_time": timezone.now() - datetime.timedelta(seconds=5), "duration": duration + 5, "auto_remove": 0})
+    start_time = timezone.now() - datetime.timedelta(seconds=5)
+    duration += 5
+
+    timer, created = timer_models.Timer.objects.get_or_create(action="auto-lightgroup-%s" % group, defaults={"name": GROUP_TIMER_NAME_MAP[group], "start_time": start_time, "duration": duration, "auto_remove": 0})
     if not created:
-        timer.start_time = timezone.now() - datetime.timedelta(seconds=5)
+        timer.start_time = start_time
         timer.duration = duration
+    logger.info("Updated timer for group %s: start_time=%s, duration=%s", group, start_time, duration)
     timer.save()
 
 def delete_group_automatic_timer(group, no_actions=False):
