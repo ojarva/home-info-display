a28826a0b57742d3cb2ac57c0a17b37f2afff302
Olli Jarva <olli@jarva.fi>
2015-03-21 10:21:34 +0200

    Use logging. Remove hardcoded settings

--- homedisplay/control_milight/management/commands/listen_433.py
+++ homedisplay/control_milight/management/commands/listen_433.py
@@ -3,34 +3,29 @@ from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 import serial
 import time
+import logging
+
+logger = logging.getLogger("%s.%s" % ("homecontroller", __name__))
 
 class Command(BaseCommand):
     args = ''
     help = 'Listen for 433MHz radio messages'
 
-    ITEM_MAP = {
-        "5236713": "kitchen",
-        "7697747": "hall",
-        "1328959": "front-door",
-        "247615": "unused-magnetic-switch",
-        "8981913": "table",
-    }
-
     def handle(self, *args, **options):
         s = serial.Serial(settings.ARDUINO_433, 9600)
+        ITEM_MAP = settings.ARDUINO_433_ITEM_MAP
         sent_event_map = {}
         while True:
             line = s.readline()
-            print "- %s" % line
             if line.startswith("Received "):
                 id = line.split(" ")[1]
                 if id in self.ITEM_MAP:
                     item_name = self.ITEM_MAP[id]
                     if item_name in sent_event_map:
                         if sent_event_map[item_name] > time.time() - 5:
-                            print "Too recent event: %s" % item_name
                             continue
+                    logger.info("Processing trigger %s (%s)", item_name, id)
                     process_automatic_trigger(item_name)
                     sent_event_map[item_name] = time.time()
                 else:
-                    print "Unknown id: %s" % id
+                    logger.warn("Unknown ID: %s", id)
