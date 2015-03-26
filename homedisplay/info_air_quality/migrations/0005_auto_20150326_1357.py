3a06957caba35d49393ed81462be9ff170d45182
Olli Jarva <olli@jarva.fi>
2015-03-26 14:04:14 +0200

    Update sorting options

---
+++ homedisplay/info_air_quality/migrations/0005_auto_20150326_1357.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('info_air_quality', '0004_auto_20150322_1609'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='airdatapoint',
+            options={'ordering': ['-timepoint__timestamp'], 'get_latest_by': 'timepoint__timestamp'},
+        ),
+        migrations.AlterModelOptions(
+            name='airtimepoint',
+            options={'ordering': ['-timestamp'], 'get_latest_by': 'timestamp'},
+        ),
+        migrations.AlterModelOptions(
+            name='outsideairquality',
+            options={'ordering': ['-timestamp'], 'get_latest_by': 'timestamp'},
+        ),
+    ]
