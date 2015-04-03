c577755016bac1b3e4cc43c6025a610c7d4ee5fe
Olli Jarva <olli@jarva.fi>
2015-04-03 13:49:24 +0300

    Send appropriate headers when fetching weather information

--- homedisplay/info_weather/management/commands/fetch_fmi_weather.py
+++ homedisplay/info_weather/management/commands/fetch_fmi_weather.py
@@ -14,18 +14,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         redis_instance = redis.StrictRedis()
 
+        headers = {
+            "Accept-Encoding": None,
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36",
+        }
+
         # Fetch warnings - Sörnäinen
-        response = requests.get("http://m.fmi.fi/mobile/interfaces/warnings.php?l=fi&version=1.1.10&preventcache=%s654" % time.time())
+        response = requests.get("http://m.fmi.fi/mobile/interfaces/warnings.php?l=fi&version=1.1.10&preventcache=%s654" % time.time(), headers=headers)
         redis_instance.setex("weather-main-warnings", 7200, response.text)
         main_warnings = response.json()
 
         # Fetch forecasts
-        response = requests.get("http://m.fmi.fi/mobile/interfaces/weatherdata.php?locations=636242&l=fi&version=1.1.10&preventcache=%s070" % time.time())
+        response = requests.get("http://m.fmi.fi/mobile/interfaces/weatherdata.php?locations=636242&l=fi&version=1.1.10&preventcache=%s070" % time.time(), headers=headers)
         redis_instance.setex("weather-main-forecasts", 7200, response.text)
         main_forecasts = response.json()
 
         # Harmaja forecasts
-        response = requests.get("http://m.fmi.fi/mobile/interfaces/weatherdata.php?locations=658802&l=en&version=1.1.10&preventcache=%s842" % time.time())
+        response = requests.get("http://m.fmi.fi/mobile/interfaces/weatherdata.php?locations=658802&l=en&version=1.1.10&preventcache=%s842" % time.time(), headers=headers)
         redis_instance.setex("weather-marine-forecasts", 7200, response.text)
         marine_forecasts = response.json()
 
