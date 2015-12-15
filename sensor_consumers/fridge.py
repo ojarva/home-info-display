6d575b3334690a1eb4fb4e328ad7b36176e55638
Olli Jarva <olli@jarva.fi>
2015-12-15 17:22:33 +0200

    Add fridge power consumption metering

---
+++ sensor_consumers/fridge.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+
+from local_settings import *
+from utils import SensorConsumerBase
+import redis
+import datetime
+import sys
+
+class Fridge(SensorConsumerBase):
+    def __init__(self):
+        SensorConsumerBase.__init__(self, "indoor_air_quality")
+
+    def run(self):
+        self.subscribe("fridgetop-pubsub", self.pubsub_callback)
+
+    def pubsub_callback(self, data):
+        if "action" in data:
+            return
+
+        self.insert_into_influx([{
+            "measurement": "fridge",
+            "time": datetime.datetime.utcnow().isoformat() + "Z",
+            "fields": {
+                "power_consumption": round(data["data"]["power_consumption"], 3),
+                "kitchen_ceiling_temperature": round(data["data"]["room_temperature"], 1),
+            }
+        }])
+
+def main():
+    item = Fridge()
+    item.run()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
