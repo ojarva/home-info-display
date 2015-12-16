6c4f7ef766df2f0a63857357fac811fcbf63c597
Olli Jarva <olli@jarva.fi>
2015-12-16 12:14:04 +0200

    Update microwave on/off logic

--- sensor_consumers/microwave.py
+++ sensor_consumers/microwave.py
@@ -6,11 +6,18 @@ import redis
 import datetime
 import sys
 
+"""
+- ~34W -> light on
+- ~1W -> off
+- >120W -> running
+"""
+
 class Microwave(SensorConsumerBase):
     def __init__(self):
         SensorConsumerBase.__init__(self, "indoor_air_quality")
         self.on_since = None
         self.door_opened_after_use = True
+        self.light_on_set = False
         self.last_used_at = None
 
     def run(self):
@@ -30,24 +37,35 @@ class Microwave(SensorConsumerBase):
                 "power_consumption": round(data["data"]["power_consumption"], 3),
                 "door": door_open,
             }
-            }])
+        }])
+
         if door_open:
-            # Must be off, as the door is open
             print "Door is open"
-            self.delete_notification("microwave")
-            self.door_opened_after_use = True
+
+            # Must be off, since the door is open
             self.on_since = None
+
+            if data["data"]["power_consumption"] > 0.08 and not self.light_on_set:
+                # Door is open, microwave is not running but still consumes energy -> timer is not set to zero.
+                self.update_notification("microwave", "Mikron valo päällä", True)
+                self.light_on_set = True
+            else:
+                self.delete_notification("microwave")
+            self.door_opened_after_use = True
             return
-        if data["data"]["power_consumption"] > 1:
+        if data["data"]["power_consumption"] > 0.2:
             if not self.on_since:
                 self.on_since = datetime.datetime.now()
             self.update_notification("microwave", "Mikro päällä ({elapsed_since})", False, elapsed_since=self.on_since)
             self.last_used_at = datetime.datetime.now()
             self.door_opened_after_use = False
+            self.light_on_set = False
             return
 
         # Not running, but door is not open either
         self.on_since = None
+
+        # If microwave has been used, but door has not been opened, there should be something inside.
         if self.last_used_at and not self.door_opened_after_use:
             self.update_notification("microwave", "Mikrossa kamaa ({from_now_timestamp})", True, from_now_timestamp=self.last_used_at)
 
