84e5650ebfdafbd499023161dd66e639c8aa0d8c
Olli Jarva <olli@jarva.fi>
2016-02-04 19:10:15 +0200

    Formatting fixes

--- homedisplay/node_data_receiver/views.py
+++ homedisplay/node_data_receiver/views.py
@@ -9,29 +9,37 @@ redis_instance = redis.StrictRedis()
 
 class Fridge(View):
     def get(self, request, *args, **kwargs):
+        def format_number(number):
+            if number is None:
+                return None
+            else:
+                return float(number)
         fridge_humidity = None
         fridge_temperature4 = None
-        humidity_data = kwargs.get("hd")
+        humidity_data = request.GET.get("hd")
         if humidity_data and len(humidity_data) > 0:
             if humidity_data[0] == "B":
                 humidity_data = humidity_data.split("T")
                 fridge_humidity = int(humidity_data[0][1:])
                 fridge_temperature4 = float(humidity_data[1]) / 100
-        fridge = kwargs.get("fridge")
-        freezer = kwargs.get("freezer")
+        fridge = request.GET.get("fridge")
+        freezer = request.GET.get("freezer")
         if fridge is not None:
             fridge = fridge == "1"
         if freezer is not None:
             freezer = freezer == "1"
 
         data = {
-            "fridge_door_open": fridge,
-            "freezer_door_open": freezer,
-            "fridge_temperature1": kwargs.get("t1"),
-            "fridge_temperature2": kwargs.get("t2"),
-            "fridge_temperature3": kwargs.get("t3"),
-            "freezer_temperature1": kwargs.get("t_freezer1"),
-            "humidity_data": kwargs.get("hd")
+            "data": {
+                "fridge_door_open": fridge,
+                "freezer_door_open": freezer,
+                "fridge_temperature1": format_number(request.GET.get("t1")),
+                "fridge_temperature2": format_number(request.GET.get("t2")),
+                "fridge_temperature3": format_number(request.GET.get("t3")),
+                "freezer_temperature1": format_number(request.GET.get("t_freezer1")),
+                "fridge_humidity": fridge_humidity,
+                "fridge_temperature4": fridge_temperature4,
+            }
         }
 
         redis_instance.publish("fridge-inside-pubsub", json.dumps(data))
