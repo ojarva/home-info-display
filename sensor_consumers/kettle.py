d6a9098e272689759eea20b07b413730fc803c16
Olli Jarva <olli@jarva.fi>
2016-02-13 15:44:24 +0200

    Add notifications and eta for kettle

--- sensor_consumers/kettle.py
+++ sensor_consumers/kettle.py
@@ -5,7 +5,6 @@ import ikettle2
 import json
 import redis
 import select
-import subprocess
 import time
 
 def listen_kettle_commands(queue):
@@ -19,13 +18,16 @@ def listen_kettle_commands(queue):
                 item = json.loads(item["data"])
                 queue.put(item)
 
-class Kettle:
+class Kettle(SensorConsumerBase):
     def __init__(self):
+        SensorConsumerBase.__init__(self, "indoor_air_quality")
         self.r = redis.StrictRedis()
         self.latest_status = None
         self.temperatures = []
-        self.sensor_base = SensorConsumerBase("indoor_air_quality")
         self.last_update_at = 0
+        self.delete_notification("kettle")
+        self.boil_to = None
+        self.boiled_at = None
 
     def add_temperature(self, temperature):
         if temperature > 101:
@@ -39,6 +41,13 @@ class Kettle:
         if len(self.temperatures) > 0:
             return sum(self.temperatures) / len(self.temperatures)
         return None
+
+    @classmethod
+    def get_eta(cls, current_temperature, destination_temperature):
+        diff = destination_temperature - current_temperature
+        if diff < 0:
+            return None
+        return diff * 2 # about one degree per two seconds
 
     def run(self):
         commands_queue = Queue()
@@ -61,9 +70,24 @@ class Kettle:
                 self.r.setex("kettle-info", 60, json.dumps(self.latest_status))
                 self.r.publish("home:broadcast:generic", json.dumps({"key": "kettle-info", "content": self.latest_status}))
 
+                if self.latest_status["status"] == "boiling" and self.boil_to:
+                    eta = self.get_eta(self.latest_status["temperature"], self.boil_to)
+                    if eta is None:
+                        eta = "?"
+                    else:
+                        eta = "%ss" % eta
+                    self.update_notification("kettle", "Vesi {} - {}, ETA {}".format(self.latest_status["temperature"], self.boil_to, eta), False)
+                    self.boiled_at = datetime.datetime.now()
+                else:
+                    if self.boiled_at and datetime.datetime.now() - self.boiled_at < datetime.timedelta(seconds=30):
+                        self.update_notification("kettle", "Vesi valmis ({} - {})".format(self.latest_status["temperature"], self.boil_to), False)
+                    else:
+                        self.delete_notification("kettle")
+                        self.boil_to = None
+
                 if time.time() - self.last_update_at > 10:
                     self.last_update_at = time.time()
-                    self.sensor_base.insert_into_influx([{
+                    self.insert_into_influx([{
                         "measurement": "kettle",
                         "time": datetime.datetime.utcnow().isoformat() + "Z",
                         "fields": {
@@ -88,8 +112,11 @@ class Kettle:
                 if self.latest_status["water_level"] in ("empty", "too_low"):
                     print "Can't turn on - not enough water"
                     continue
-                kettle.on(command.get("on", 100))
+                destination_temperature = command.get("on", 100)
+                self.boil_to = destination_temperature
+                kettle.on(destination_temperature)
             elif command.get("off"):
+                self.boiled_at = None
                 kettle.off()
 
 def main():
