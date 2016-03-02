9cfe1a685eeb65506df4e2983f4b4aa77979202f
Olli Jarva <olli@jarva.fi>
2016-03-02 09:51:20 +0200

    Fix json encoder

--- homedisplay/control_milight/management/commands/listen_433.py
+++ homedisplay/control_milight/management/commands/listen_433.py
@@ -14,7 +14,7 @@ logger = logging.getLogger("%s.%s" % ("homecontroller", __name__))
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, o):
-        if isinstance(o, datetime):
+        if isinstance(o, datetime.datetime):
             return o.isoformat()
 
         return json.JSONEncoder.default(self, o)
