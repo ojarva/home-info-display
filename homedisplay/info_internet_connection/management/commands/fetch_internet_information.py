935be7f72dce4d9a115511e0f143ad79c37a07ee
Olli Jarva <olli@jarva.fi>
2016-03-06 22:02:27 +0200

    Push modem status to influxdb

--- homedisplay/info_internet_connection/management/commands/fetch_internet_information.py
+++ homedisplay/info_internet_connection/management/commands/fetch_internet_information.py
@@ -1,13 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from homedisplay.utils import publish_ws
+from influxdb import InfluxDBClient
 from info_internet_connection.models import Internet, get_latest_serialized
 import datetime
 import huawei_b593_status
 import json
+import redis
 
 
-#{'WIFI': 'off', 'SIG': '5', 'Mode': '4g', 'Roam': 'home', 'SIM': 'normal', 'Connect': 'connected'}
+# {'WIFI': 'off', 'SIG': '5', 'Mode': '4g', 'Roam': 'home', 'SIM': 'normal', 'Connect': 'connected'}
+
+class DateTimeEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return o.isoformat()
+
+        return json.JSONEncoder.default(self, o)
 
 
 class Command(BaseCommand):
@@ -33,4 +43,24 @@ class Command(BaseCommand):
         latest_data.connect_status = data["Connect"]
         latest_data.update_timestamp = timezone.now()
         latest_data.save()
+
+        influx_data = {
+            "timestamp": timezone.now(),
+            "measurement": "4g-modem",
+            "tags": {
+                "device": "home-gw",
+            },
+            "fields": {
+                "wifi": data["WIFI"] != "off",
+                "signal": int(data["SIG"]),
+                "mode": data["Mode"],
+                "sim": data["SIM"],
+                "connect_status": data["Connect"],
+            },
+        }
+        redis_instance = redis.StrictRedis()
+        redis_instance.publish("influx-update-pubsub", json.dumps(data, cls=DateTimeEncoder))
         publish_ws("internet", get_latest_serialized())
+
+        influx_client = InfluxDBClient("localhost", 8086, "root", "root", "home")
+        influx_client.write_points(influx_data)
