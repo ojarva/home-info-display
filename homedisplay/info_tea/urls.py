cf6acff49ede388e782401511e1cef8688f6bd15
Olli Jarva <olli@jarva.fi>
2016-03-30 20:20:36 +0300

    Fix NFC tag URLs

--- homedisplay/info_tea/urls.py
+++ homedisplay/info_tea/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^list$', views.list.as_view()),
-    url(r'^get/(?P<id>([a-z0-9_-]+))$', views.item.as_view()),
-    url(r'^item/(?P<id>([:a-z0-9_-]+))$', views.get_or_create.as_view()),
+    url(r'^get/(?P<id>([:A-Za-z0-9_-]+))$', views.item.as_view()),
+    url(r'^item/(?P<id>([:A-Za-z0-9_-]+))$', views.get_or_create.as_view()),
 ]
